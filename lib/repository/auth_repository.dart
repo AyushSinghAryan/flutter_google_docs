d3707c27d230c9be581a5f960cb987dd2117446b
Ayush Singh Aryan <ayush.singh2019@vitbhopal.ac.in>
2024-06-23 16:39:33 +0530

    Day-1

---
+++ lib/repository/auth_repository.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final authRepositoryProvider = Provider(
+  (ref) => AuthRepository(
+    googleSignIn: GoogleSignIn(),
+  ),
+);
+// this provider object allow us to interact with other providers
+
+//! pass instance of class to this provider
+class AuthRepository {
+  final GoogleSignIn _googleSignIn;
+  AuthRepository({
+    required GoogleSignIn googleSignIn,
+    // we can not access private variable through constructor
+  }) : _googleSignIn = googleSignIn;
+
+  void signInWithGoogle() async {
+    try {
+      final user = await _googleSignIn.signIn();
+      if (user != null) {
+        print(user.email);
+        print(user.displayName);
+        print(user.photoUrl);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+}
